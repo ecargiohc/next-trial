c1b8042a2a818d643dc0912d996187333af76e90
github username <therealgracechoi@gmail.com>
2020-05-20 11:58:31 -0700

    testing fancy routes with another page/about.js

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0023519b126b105e140d";
+/******/ 	var hotCurrentHash = "8628b99d749f167aa135";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ pages/about.js
@@ -0,0 +1,8 @@
+const anotherFunc = () => {
+    return (
+        <h1>
+            hello from About.js
+        </h1>
+    )
+};
+export default anotherFunc;
