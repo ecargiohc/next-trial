085fdb4b1d785f31f414f9d3a5af90b68dd2dd6a
github username <therealgracechoi@gmail.com>
2020-06-02 10:15:28 -0700

    to note: _app.js is a file that you can override and belongs in the 'pages' directory; (bc it comes with nextjs), all of the components get imported and rendered inside of _app.js component

--- pages/about.js
+++ pages/about.js
@@ -2,9 +2,7 @@ import Navbar from '../components/Navbar'
 const anotherFunc = () => {
     console.log("***running ABOUT component***")
     return (
-        <>
-            hello from About.js
-        </>
+            <h1>hello from About.js</h1>
     )
 };
 export default anotherFunc;
--- pages/contact.js
+++ pages/contact.js
@@ -2,8 +2,7 @@ import Navbar from '../components/Navbar'
  const ContactPage = () => {
     console.log("***running CONTACT component***")
      return (
-     <>
          <h1>Contact page says hello</h1>
-     </>)
+     )
  };
  export default ContactPage;
